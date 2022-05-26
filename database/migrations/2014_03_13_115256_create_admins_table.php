b4f49ca6e7e28dee5b8bac9e95cb3157440b24c8
Raghdaa99 <roroter999@gmail.com>
2022-05-26 22:43:38 +0300

    permission-read-students2

--- database/migrations/2014_03_13_115256_create_admins_table.php
+++ database/migrations/2014_03_13_115256_create_admins_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('admins', function (Blueprint $table) {
-            $table->id('admin_no')->autoIncrement();
+            $table->id('admin_no');
             $table->string('name');
 //            $table->string('email')->unique();
 //            $table->timestamp('email_verified_at')->nullable();
